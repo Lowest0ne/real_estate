e6df11f7bb903d749d95b73e2d57b48b17394e81
Carl Schwope <schwope.carl@gmail.com>
2013-09-26 13:56:21 -0400

    refactor with State class

--- app/models/building.rb
+++ app/models/building.rb
@@ -5,11 +5,7 @@ class Building < ActiveRecord::Base
   validates_presence_of :state
   validates_presence_of :postal_code
 
-
-  def self.valid_states
-    ['MA']
-  end
-  validates_inclusion_of :state, in: Building.valid_states
+  validates_inclusion_of :state, in: State.valid_states
 
   validates_numericality_of :postal_code
   validates_length_of :postal_code, is: 5
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :building do
     street_address '33 Harrison ave'
     city 'Boston'
-    state 'MA'
+    state 'Massachusetts'
     postal_code '02111'
     description 'a hotspot for tech growth in Boston'
   end
